7e71fef0158f2d0a6240850e1560f3374eab00a1
Omar Crosby <omar.crosby@gmail.com>
2023-09-30 16:27:32 -0400

    ci: getting semantic-release updating version

---
+++ scripts/update_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Determine the new version from Semantic Release (adjust as needed)
+NEW_VERSION=$(semantic-release-cli print-version)
+
+# Update the VERSION constant in main.go using sed
+sed -i "s/const VERSION = \".*\"/const VERSION = \"$NEW_VERSION\"/" main.go
+
+# Commit the updated main.go file
+git add main.go
+git commit -m "chore: Update VERSION to $NEW_VERSION in main.go"
